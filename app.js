ae1d94816873dd784f534984d9fcb0eea7866bdf
steve <dilakshanjeganathan@gmail.com>
2026-02-10 01:09:25 +0530

    hi

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path=require("path")
 const cors=require("cors")
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://portfolio-55d37.web.app/",
     credentials:true,
     methods:["GET", "POST", "PUT", "DELETE"],
 }  
