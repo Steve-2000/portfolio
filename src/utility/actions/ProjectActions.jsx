88aecf4659f91eae6d60455ebddbca58c7a858ac
steve <dilakshanjeganathan@gmail.com>
2026-02-10 08:58:34 +0530

    fial i thik

--- src/utility/actions/ProjectActions.jsx
+++ src/utility/actions/ProjectActions.jsx
@@ -1,10 +1,12 @@
 import axios from "axios"; 
 import { projectsRequest, projectsSuccess, projectfailure } from "../slices/projectslices";
 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '';
+
 export const getprojects = () => async (dispatch) => {
     dispatch(projectsRequest());
     try{
-        const {data} = await axios.get("/api/project/projectDetail");
+        const {data} = await axios.get(`${API_BASE_URL}/api/project/projectDetail`);
         dispatch(projectsSuccess(data));
     }catch(error){
         const err = error.response && error.response.data ? error.response.data.message : error.message;
